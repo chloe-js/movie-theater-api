046aea6ba57233039878afe47627d3b6735a34ef
chloë js <91985648+chloe-js@users.noreply.github.com>
2022-11-11 10:12:16 +0000

    add GET requests for users

--- routes/user.js
+++ routes/user.js
@@ -4,5 +4,44 @@ const User = require("../models/User");
 const Show = require("../models/Show");
 
 // Users
+//POST new user 
+//POST// http://localhost:3000/user/addNewUser
+// {
+//     "id": 3,
+//     "username": "testUser3@gmail.com",
+//     "password": "ThisIsC"
+// }
+userRouter.post("/addNewUser", async (req, res) => {
+    const user = await User.User.create(req.body);
+    res.status(200).send(user);
+  });
+
+// GET all users /users
+// GET // http://localhost:3000/user/allUsers
+userRouter.get("/allUsers", async (req, res) => {
+    const user = await User.User.findAll()
+    res.status(200).send(user);
+})
+// GET one user /users/1
+// GET // http://localhost:3000/user/allUsers/2
+userRouter.get("/allUsers/:id", async (req, res) => {
+    const user = await User.User.findByPk(req.params.id)
+    res.status(200).send(user);
+});
+
+
+// GET all shows watched by a user (user id in req.params)   /users/2/shows(watched)
+//GET // http://localhost:3000/user/allUsers/1/shows-watched/watched
+userRouter.get("/allUsers/:UserId/shows-watched/:status", async (req, res) => {
+    // const user = await Show.Show.findByPk(req.params.UserId);
+    const user = await Show.Show.findAll({
+        where: {
+            UserId: req.params.UserId, // 1
+            status: req.params.status  // on-going
+        }
+      })
+    res.status(200).send(user);
+});
+
 
 module.exports = userRouter;
