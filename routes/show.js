d8657159fd0e1ed084ed8c51cc29012e13e88b7b
chloë js <91985648+chloe-js@users.noreply.github.com>
2022-11-11 10:10:57 +0000

    link user and show to routes and server

---
+++ routes/show.js
@@ -0,0 +1,7 @@
+const { Router } = require("express");
+const showRouter = Router();
+const User = require("../models/User");
+const Show = require("../models/Show");
+
+// Shows
+module.exports = showRouter;
---
+++ routes/user.js
@@ -0,0 +1,8 @@
+const { Router } = require("express");
+const userRouter = Router();
+const User = require("../models/User");
+const Show = require("../models/Show");
+
+// Users
+
+module.exports = userRouter;
