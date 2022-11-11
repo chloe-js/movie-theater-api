e90d4fa5e718777f5f464a5dfa8b9ddb72b0a04d
chloë js <91985648+chloe-js@users.noreply.github.com>
2022-11-11 14:29:43 +0000

    added validation for password and status and rating

--- routes/show.js
+++ routes/show.js
@@ -2,10 +2,11 @@ const { Router } = require("express");
 const showRouter = Router();
 const User = require("../models/User");
 const Show = require("../models/Show");
+const { body, validationResult, param } = require('express-validator')
 
 // Shows
 
-//POST new show *
+//POST new show 
 // {
 //   "title": "TestShow",
 //   "genre": "Horror",
@@ -19,19 +20,14 @@ showRouter.post("/addNewShow", async (req, res) => {
   res.status(200).send(show);
 });
 
-// GET all shows /shows *
+// GET all shows /shows 
 // GET // http://localhost:3000/show/shows
 showRouter.get("/shows", async (req, res) => {
   const show = await Show.Show.findAll()
   res.status(200).send(show);
 })
 
-// GET one show /shows/5 *
-// // http://localhost:3000/show/allShows/3
-// showRouter.get("/allShows/:id", async (req, res) => {
-//   const show = await Show.Show.findByPk(req.params.id)
-//   res.status(200).send(show);
-// });
+// GET one show /shows/5 
 // GET // http://localhost:3000/show/shows/title/X-Files
 showRouter.get("/shows/title/:title", async (req, res) => {
   const show = await Show.Show.findAll({
@@ -42,7 +38,7 @@ showRouter.get("/shows/title/:title", async (req, res) => {
   res.status(200).send(show);
 });
 
-// GET shows of a particular genre (genre in req.params) /shows/genres/Comedy *
+// GET shows of a particular genre (genre in req.params) /shows/genres/Comedy 
 // GET // http://localhost:3000/show/shows/genre/Sitcom
 showRouter.get("/shows/genre/:genre", async (req, res) => {
   // const show = await Show.Show.findByPk(req.params.genre)
@@ -55,28 +51,48 @@ showRouter.get("/shows/genre/:genre", async (req, res) => {
   res.status(200).send(show);
 });
 
-// PUT update the status of a show /shows/3/updates(rating) *
+// PUT update the status of a show /shows/3/updates(rating) 
 // PUT // http://localhost:3000/show/shows/8/update-status/cancelled 
-showRouter.put("/shows/:id/update-status/:status", async (req, res) => {
-  const show = await Show.Show.findByPk(req.params.id)
-  await show.update({status: req.params.status})
-  res.status(200).send(show);
-});
+// PUT x // http://localhost:3000/show/shows/8/update-status/can
+showRouter.put(
+  "/shows/:id/update-status/:status",
+  //check that status is not empty and char min 5 - 25 and no white space
+  param('status').isLength({ min: 5, max: 20 }),
+  async (req, res) => {
+    const err = validationResult(req)
+    // const result = req.params.status > 5 && req.params.status < 25
+    console.log(err.isEmpty());
+    if (!err.isEmpty()) {
+      return res.status(400).send(err)
+    }
+    const show = await Show.Show.findByPk(req.params.id)
+    await show.update({ status: req.params.status })
+    res.status(200).send(show);
+  });
 
-// PUT update rating of a show that has been watched /shows/4/watched *
+// PUT update rating of a show that has been watched /shows/4/watched 
 //PUT // http://localhost:3000/show/shows/3/update-rating/5
-showRouter.put("/shows/:id/update-rating/:rating", async (req, res) => {
-  const show = await Show.Show.findByPk(req.params.id)
-  await show.update({rating: req.params.rating})
-  res.status(200).send(show);
-});
+//PUT x // http://localhost:3000/show/shows/3/update-rating/9
+showRouter.put(
+  "/shows/:id/update-rating/:rating",
+  //check that rating is not empty
+  param('rating').isInt({ min: 0, max: 5 }),
+  async (req, res) => {
+    const err = validationResult(req)
+    if (!err.isEmpty()) {
+      return res.status(400).send(err)
+    }
+    const show = await Show.Show.findByPk(req.params.id)
+    await show.update({ rating: req.params.rating })
+    res.status(200).send(show);
+  });
 
 
 // DELETE a show *
 //DELETE // http://localhost:3000/show/shows/deleteShow/12
 showRouter.delete("/shows/deleteShow/:id", async (req, res) => {
   const show = await Show.Show.findByPk(req.params.id)
-  await show.destroy({id: req.params.id})
+  await show.destroy({ id: req.params.id })
   res.status(200).send(show);
   // res.status(404).send(show);
 });
--- routes/user.js
+++ routes/user.js
@@ -2,48 +2,45 @@ const { Router } = require("express");
 const userRouter = Router();
 const User = require("../models/User");
 const Show = require("../models/Show");
+const { body, validationResult } = require('express-validator')
 
 // Users
-//POST new user *
-//POST// http://localhost:3000/user/addNewUser
+//POST new user 
 // {
 //     "id": 3,
 //     "username": "testUser3@gmail.com",
 //     "password": "ThisIsC"
 // }
-userRouter.post("/addNewUser", async (req, res) => {
+//POST// http://localhost:3000/user/addNewUser
+//POST// http://localhost:3000/user/addNewUser
+userRouter.post(
+    "/addNewUser", 
+    // password is valid
+    body('username').isEmail(),
+    async (req, res) => {
+        const errors = validationResult(req)
+        if(!errors.isEmpty()){
+            return res.status(400).send({errors: errors.array()})
+        }
+
     const user = await User.User.create(req.body);
     res.status(200).send(user);
-  });
+});
 
-// GET all users /users *
+// GET all users /users 
 // GET // http://localhost:3000/user/users
 userRouter.get("/users", async (req, res) => {
     const user = await User.User.findAll()
     res.status(200).send(user);
 })
-// GET one user /users/1 *
+// GET one user /users/1 
 // GET // http://localhost:3000/user/users/2
 userRouter.get("/users/:id", async (req, res) => {
     const user = await User.User.findByPk(req.params.id)
     res.status(200).send(user);
 });
 
-
-// GET all shows watched by a user (user id in req.params)   /users/2/shows(watched) *
-//GET // http://localhost:3000/user/users/2/shows/watched
-userRouter.get("/users/:UserId/shows/:status", async (req, res) => {
-    // const user = await Show.Show.findByPk(req.params.UserId);
-    const user = await Show.Show.findAll({
-        where: {
-            UserId: req.params.UserId, // 1
-            status: req.params.status  // on-going
-        }
-      })
-    res.status(200).send(user);
-});
-
-// PUT update and add a show if a user has watched it  /users/2/shows/9 *
+// PUT update and add a show if a user has watched it  /users/2/shows/9 
 //PUT // http://localhost:3000/user/shows/2/show/7/update-watched/watched
 userRouter.put("/shows/:UserId/show/:showId/update-watched/:status", async (req, res) => {
     const user = await Show.Show.findByPk(req.params.showId)
@@ -54,34 +51,17 @@ userRouter.put("/shows/:UserId/show/:showId/update-watched/:status", async (req,
     res.status(200).send(user);
 });
 
-
-
-// userRouter.post("/", async (req, res) => {
-//     //create the user db entry
-//     // if going to await function need to async the (req,res) function 
-//     //make sure the body send with the request MATCHES the model user,js!
-//     const user = await User.create(req.body);
-//     // very important to send something in the send ()
-//     res.status(200).send({ user });
-//   });
-
-
-// userRouter.post(
-//   "/",
-//   //check email is email
-//   body('email').isEmail(),
-//   //check password length
-//   body('password').isLength({ min: 6}),
-//   async (req, res) => {
-
-//     const errors = validatorResult(req)
-//     if (!errors.isEmpty()) {
-//       return res.status(400).send({ errors: errors.array() })
-//     }
-
-//     const user = await User.create(req.body);
-//     res.status(201).send({ user });
-//   }
-// );
+// GET all shows watched by a user (user id in req.params)   /users/2/shows(watched) 
+//GET // http://localhost:3000/user/users/2/shows/watched
+userRouter.get("/users/:UserId/shows/:status", async (req, res) => {
+    // const user = await Show.Show.findByPk(req.params.UserId);
+    const user = await Show.Show.findAll({
+        where: {
+            UserId: req.params.UserId, // 1
+            status: req.params.status  // on-going
+        }
+    })
+    res.status(200).send(user);
+});
 
 module.exports = userRouter;
